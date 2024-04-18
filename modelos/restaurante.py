ed48516cc5a4b76a732f0af398579afcfa2faa4d
Cezar Tosta <cezar.tosta@hotmail.com>
2024-04-18 01:11:27 -0300

    Verificando funções especiais
     dir( ) e vars( )

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -4,4 +4,10 @@ class Restaurante():
     ativo = False
 
 restaurante_praca = Restaurante()
-restaurante_pizza = Restaurante()
+restaurante_praca.nome = 'Praça'
+restaurante_praca.categoria = 'Gourmet'
+
+# Mostrando os atributos de uma Classe
+print(dir(restaurante_praca))
+# Mostra um dicionário com chaves e valores
+print(vars(restaurante_praca))
