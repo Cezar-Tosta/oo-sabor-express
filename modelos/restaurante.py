f7e0e494949f69af2e832ca703739bc885f62c3a
Cezar Tosta <cezar.tosta@hotmail.com>
2024-04-18 13:10:52 -0300

    Função alternar_estado()
    Criando a função alternar_estado() e demais ajustes no código

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -10,17 +10,22 @@ class Restaurante():
     def __str__(self): # Método especial
         return f'{self._nome} | {self._categoria}'
     
-    def listar_restaurantes():
+    @classmethod # Informando que é um método da classe
+    def listar_restaurantes(cls):
         print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(15)} | {'Status'}')
-        for rest in Restaurante.restaurantes:
+        for rest in cls.restaurantes:
             print(f'{rest._nome.ljust(25)} | {rest._categoria.ljust(15)} | {rest.ativo}')
 
     @property # Modifica como um atributo é lido
     def ativo(self):
         return '▣' if self._ativo else '▢'
+    
+    def alternar_estado(self):
+        self._ativo = not self._ativo
 
 
 restaurante_praca = Restaurante('praça restaurante', 'gourmet')
+restaurante_praca.alternar_estado()
 restaurante_pizza = Restaurante('pizza express', 'italiana')
 
 Restaurante.listar_restaurantes()
