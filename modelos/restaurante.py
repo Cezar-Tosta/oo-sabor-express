6266af33f5bb3d89fe995bcc6cb6076424f12891
Cezar Tosta <cezar.tosta@hotmail.com>
2024-04-18 11:27:57 -0300

    Método especial __str__
    Método __str__ e demais ajustes

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,13 +1,18 @@
 class Restaurante():
-    def __init__(self, nome, categoria): # Construtor
+    def __init__(self, nome, categoria): # Construtor (método especial)
         self.nome = nome
         self.categoria = categoria
         self.ativo = False
 
-restaurante_praca = Restaurante('Praça', 'Gourmet')
+    def __str__(self): # Método especial
+        return f'{self.nome} | {self.categoria}'
+
+restaurante_praca = Restaurante('Praça Restaurante', 'Gourmet')
 restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
 print(vars(restaurante_praca))
 print(vars(restaurante_pizza))
+print(restaurante_pizza)
+print(restaurante_praca)
 
 '''
 # Mostrando os atributos de uma Classe
