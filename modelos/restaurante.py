e6e5a4504a35303f289b210f0f47c7585274e245
Cezar Tosta <cezar.tosta@hotmail.com>
2024-04-18 11:33:59 -0300

    Criação do método listar_restaurantes()
    Criação de listar_restaurantes() e ajustes no código

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,22 +1,21 @@
 class Restaurante():
+    restaurantes = []
+
     def __init__(self, nome, categoria): # Construtor (método especial)
         self.nome = nome
         self.categoria = categoria
         self.ativo = False
+        Restaurante.restaurantes.append(self)
 
     def __str__(self): # Método especial
         return f'{self.nome} | {self.categoria}'
+    
+    def listar_restaurantes():
+        for rest in Restaurante.restaurantes:
+            print(f'{rest.nome} | {rest.categoria} | {rest.ativo}')
+
 
 restaurante_praca = Restaurante('Praça Restaurante', 'Gourmet')
 restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
-print(vars(restaurante_praca))
-print(vars(restaurante_pizza))
-print(restaurante_pizza)
-print(restaurante_praca)
 
-'''
-# Mostrando os atributos de uma Classe
-print(dir(restaurante_praca))
-# Mostra um dicionário com chaves e valores
-print(vars(restaurante_praca))
-'''
+Restaurante.listar_restaurantes()
