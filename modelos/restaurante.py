da345c4327c3a5b55a89ef2595c1d49851585531
Cezar Tosta <cezar.tosta@hotmail.com>
2024-04-18 11:19:52 -0300

    Instâncias
    Começando a trabalhar com instâncias

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,13 +1,17 @@
 class Restaurante():
-    nome = ''
-    categoria = ''
-    ativo = False
+    def __init__(self, nome, categoria): # Construtor
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
 
-restaurante_praca = Restaurante()
-restaurante_praca.nome = 'Praça'
-restaurante_praca.categoria = 'Gourmet'
+restaurante_praca = Restaurante('Praça', 'Gourmet')
+restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
+print(vars(restaurante_praca))
+print(vars(restaurante_pizza))
 
+'''
 # Mostrando os atributos de uma Classe
 print(dir(restaurante_praca))
 # Mostra um dicionário com chaves e valores
 print(vars(restaurante_praca))
+'''
