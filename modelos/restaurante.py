d31f0927b198e934b1a1bffd25b676b7d58f5a30
Cezar Tosta <cezar.tosta@hotmail.com>
2024-04-18 13:00:45 -0300

    Ajustando variáveis

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -2,25 +2,25 @@ class Restaurante():
     restaurantes = []
 
     def __init__(self, nome, categoria): # Construtor (método especial)
-        self.nome = nome
-        self.categoria = categoria
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
         self._ativo = False
         Restaurante.restaurantes.append(self)
 
     def __str__(self): # Método especial
-        return f'{self.nome} | {self.categoria}'
+        return f'{self._nome} | {self._categoria}'
     
     def listar_restaurantes():
         print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(15)} | {'Status'}')
         for rest in Restaurante.restaurantes:
-            print(f'{rest.nome.ljust(25)} | {rest.categoria.ljust(15)} | {rest.ativo}')
+            print(f'{rest._nome.ljust(25)} | {rest._categoria.ljust(15)} | {rest.ativo}')
 
     @property # Modifica como um atributo é lido
     def ativo(self):
         return '▣' if self._ativo else '▢'
 
 
-restaurante_praca = Restaurante('Praça Restaurante', 'Gourmet')
-restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
+restaurante_praca = Restaurante('praça restaurante', 'gourmet')
+restaurante_pizza = Restaurante('pizza express', 'italiana')
 
 Restaurante.listar_restaurantes()
