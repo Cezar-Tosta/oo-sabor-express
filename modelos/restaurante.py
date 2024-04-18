2ba854cb6fe6cd619c54c020c6dad6841a6fc631
Cezar Tosta <cezar.tosta@hotmail.com>
2024-04-18 12:55:33 -0300

    Property
    Trabalhando com @property

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -4,15 +4,20 @@ class Restaurante():
     def __init__(self, nome, categoria): # Construtor (método especial)
         self.nome = nome
         self.categoria = categoria
-        self.ativo = False
+        self._ativo = False
         Restaurante.restaurantes.append(self)
 
     def __str__(self): # Método especial
         return f'{self.nome} | {self.categoria}'
     
     def listar_restaurantes():
+        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(15)} | {'Status'}')
         for rest in Restaurante.restaurantes:
-            print(f'{rest.nome} | {rest.categoria} | {rest.ativo}')
+            print(f'{rest.nome.ljust(25)} | {rest.categoria.ljust(15)} | {rest.ativo}')
+
+    @property # Modifica como um atributo é lido
+    def ativo(self):
+        return '▣' if self._ativo else '▢'
 
 
 restaurante_praca = Restaurante('Praça Restaurante', 'Gourmet')
