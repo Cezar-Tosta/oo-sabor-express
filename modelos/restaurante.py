35e11842592757c7a6bc72783e9fe96fb8f3a812
Cezar Tosta <cezar.tosta@hotmail.com>
2024-04-19 11:09:52 -0300

    Ajustes e docstrings
    Ajustando o código para limite das notas (0 a 5) e " - " quando não houver avaliação. Inserção de docstrings nas funções da classe Restaurante

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,39 +1,61 @@
 from modelos.avaliacao import Avaliacao
 
-class Restaurante():
+class Restaurante:
+    """Representa um restaurante e suas características."""
+
     restaurantes = []
 
-    def __init__(self, nome, categoria): # Construtor (método especial)
+    def __init__(self, nome, categoria):
+        """
+        Inicializa uma instância de Restaurante.
+
+        Parâmetros:
+        - nome (str): O nome do restaurante.
+        - categoria (str): A categoria do restaurante.
+        """
         self._nome = nome.title()
         self._categoria = categoria.upper()
         self._ativo = False
         self._avaliacao = []
         Restaurante.restaurantes.append(self)
-
-    def __str__(self): # Método especial
+    
+    def __str__(self):
+        """Retorna uma representação em string do restaurante."""
         return f'{self._nome} | {self._categoria}'
     
-    @classmethod # Informando que é um método da classe
+    @classmethod
     def listar_restaurantes(cls):
-        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(15)} | {'Avaliação'.ljust(10)} | {'Status'}')
-        for rest in cls.restaurantes:
-            print(f'{rest._nome.ljust(25)} | {rest._categoria.ljust(15)} | {str(rest.media_avaliacoes).ljust(10)} | {rest.ativo}')
+        """Exibe uma lista formatada de todos os restaurantes."""
+        print(f'{"Nome do restaurante".ljust(25)} | {"Categoria".ljust(25)} | {"Avaliação".ljust(25)} | {"Status"}')
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {str(restaurante.media_avaliacoes).ljust(25)} | {restaurante.ativo}')
 
-    @property # Modifica como um atributo é lido
+    @property
     def ativo(self):
-        return '▣' if self._ativo else '▢'
+        """Retorna um símbolo indicando o estado de atividade do restaurante."""
+        return '⌧' if self._ativo else '☐'
     
     def alternar_estado(self):
+        """Alterna o estado de atividade do restaurante."""
         self._ativo = not self._ativo
 
     def receber_avaliacao(self, cliente, nota):
-        avaliacao = Avaliacao(cliente, nota)
-        self._avaliacao.append(avaliacao)
+        """
+        Registra uma avaliação para o restaurante.
+
+        Parâmetros:
+        - cliente (str): O nome do cliente que fez a avaliação.
+        - nota (float): A nota atribuída ao restaurante (entre 1 e 5).
+        """
+        if 0 <= nota <= 5: 
+            avaliacao = Avaliacao(cliente, nota)
+            self._avaliacao.append(avaliacao)
 
     @property
     def media_avaliacoes(self):
+        """Calcula e retorna a média das avaliações do restaurante."""
         if not self._avaliacao:
-            return 0
+            return '-'
         soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
         quantidade_de_notas = len(self._avaliacao)
         media = round(soma_das_notas / quantidade_de_notas, 1)
