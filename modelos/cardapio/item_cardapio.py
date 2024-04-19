f06a8e44db5afe60cbb19d22092ed3eb0b2fc753
Cezar Tosta <cezar.tosta@hotmail.com>
2024-04-19 12:11:24 -0300

    Aprimorando o projeto
    Eliminando os arquivos extras para o projeto. Criando outros que servirão para as próximas aulas.

---
+++ modelos/cardapio/item_cardapio.py
@@ -0,0 +1,5 @@
+class ItemCardapio:
+    def __init__(self,nome,preco):
+        self._nome = nome #  O uso de _ antes de 'nome' é uma convenção em Python para indicar que essas variáveis são "privadas" ou "protegidas".
+        self._preco = preco
+    
---
+++ modelos/cardapio/prato.py
@@ -0,0 +1,6 @@
+class Prato:
+    def __init__(self,nome,preco,descricao):
+        pass
+        # self._nome = nome
+        # self._preco = preco
+        # self._descricao = descricao
