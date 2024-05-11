8b1cf9fb4ececb8275cfc0b2d003f3b6327770b0
Cezar Tosta <cezar.tosta@hotmail.com>
2024-05-10 22:29:02 -0300

    Classe Sobremesa

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from modelos.restaurante import Restaurante
 from modelos.cardapio.bebida import Bebida
 from modelos.cardapio.prato import Prato
+from modelos.cardapio.sobremesa import Sobremesa
 
 restaurante_praca = Restaurante('praça', 'gourmet')
 bebida_suco = Bebida('Suco de Melancia', 5.00, 'Grande')
@@ -9,6 +10,10 @@ prato_paozinho = Prato('Pãozinho', 2.00, 'O melhor pão da cidade')
 prato_paozinho.aplicar_desconto()
 restaurante_praca.adicionar_no_cardapio(bebida_suco)
 restaurante_praca.adicionar_no_cardapio(prato_paozinho)
+
+sobremesa_doce = Sobremesa('Doce de leite', 2.00, 'Caseiro', 'Pequeno')
+restaurante_praca.adicionar_no_cardapio(sobremesa_doce)
+sobremesa_doce.aplicar_desconto()
 
 def main():
     restaurante_praca.exibir_cardapio
